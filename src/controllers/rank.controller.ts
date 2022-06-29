01b715685d90d31e4abda6285324b6a5e02805cc
SONUIT <19522138@gm.uit.edu.vn>
2022-06-29 11:34:52 +0700

    Adjust ranking query

--- src/controllers/rank.controller.ts
+++ src/controllers/rank.controller.ts
@@ -4,16 +4,36 @@ import { Request, Response, NextFunction } from "express";
 class rankController {
   getTrackRankByDate = catchAsync(async (req: Request, res: Response) => {
     const { genre } = req.body;
-    console.log(genre);
-    const result = await Rank.find({
-      // can't not use $and in date
-      date: {
-        $lte: new Date(Date.now()), //to
-        $gte: new Date(Date.now() - 60 * 60 * 24 * 1000) //from yesterday
+    //check if have genre in body request
+    const genreOptions = genre ? genre : { $exists: true };
+    const result = await Rank.aggregate([
+      {
+        $match: {
+          date: {
+            // can't not use $and in date
+            $lte: new Date(Date.now()), //to
+            $gte: new Date(Date.now() - 60 * 60 * 24 * 1000) //from yesterday
+          },
+          genre: genreOptions
+        }
+      },
+      {
+        $group: {
+          _id: "$date",
+          results: { $push: { genre: "$genre", topRank: "$topRank" } }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          date: "$_id",
+          results: 1
+        }
       }
-    });
+    ]);
     return res.status(200).send({
       message: "success",
+      length: result.length,
       data: result
     });
   });
