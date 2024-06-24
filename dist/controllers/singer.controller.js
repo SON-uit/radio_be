58380770b0356f8eaa3b6bbb4f62ecc2ea2b47ce
Son Nguyen <sonnt.bdg@vnpt.vn>
2024-06-24 20:01:21 +0700

    demo1

--- dist/controllers/singer.controller.js
+++ dist/controllers/singer.controller.js
@@ -91,8 +91,7 @@ class SingerController {
             //   status: "Success",
             //   data: tracks
             // });
-            console.log("Test git");
-            console.log("Test git");
+            console.log("commit 1");
         }));
         this.getAllAlbumOfSinger = (0, catchAsync_1.default)((req, res, next) => __awaiter(this, void 0, void 0, function* () {
             //const tracks = await Track.find({ singers: { $elemMatch: { $eq: req.params.singerId } } });
